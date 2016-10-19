f80fb816415afe802199d07b442611764fd4fe88
Davy G <gabdavy@gmail.com>
2016-10-20 01:45:25 +0200

    publi files

---
+++ web/bundles/app/scripts/scripts-home.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    onResize();
+});
+
+$( window ).resize(function() {
+    onResize();
+});
+
+function onResize() {
+    var w_height = $( window ).height();
+    var max_height = Math.max($( "#text_home" ).height() + 100, w_height);
+    $("#wrap").height(max_height);
+    $("header").height(max_height);
+
+    var top = ((w_height - $( "#text_home" ).height()) / 1.6);
+    top = top < 70 ? 70 : top;
+    $( "#text_home" ).css({ top: top  + 'px'});
+}
