4e32ba64f0d6bce18495bbc883b603f85f1a96e9
Davy G <gabdavy@gmail.com>
2017-01-05 22:41:55 +0100

    Users admin

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -4,20 +4,16 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Component\HttpFoundation\Request;
-
 
 class AdminController extends Controller
 {
     /**
      * @Security("has_role('ROLE_ADMIN')")
      */
-    public function usersAction(Request $request) {
+    public function usersAction() {
         $repository = $this->getDoctrine()->getManager()->getRepository('AppBundle:Utilisateurs');
         $utilisateurs = $repository->findAll();
 
-        if ($request->query->get('type') == 'csv') {
-        }
         return $this->render('AppBundle:Admin:users.html.twig', array(
             'users' => $utilisateurs
         ));
